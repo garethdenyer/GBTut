2f81e6a1e2f9905629bb7ab56f15f50a4e7c40fa
Gareth Denyer <gareth.denyer@sydney.edu.au>
2022-03-06 18:47:40 +1100

    Label only shows when ball stationary

--- GB/Assets/Scripts/Ball.cs
+++ GB/Assets/Scripts/Ball.cs
@@ -9,6 +9,28 @@ public class Ball : MonoBehaviour
     public string ballID; //R1 to W10
     public GameObject ballstamp; //the physical mesh that forms the number on the ball 
     public TMP_Text balllabel; //number displayed on the top of the ball when stationary
+    public GameObject label;
 
+    Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //only show the info panel while ball is stationary
+        if (rb.velocity.x > 0 || rb.velocity.z > 0)
+        {
+            label.SetActive(false);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            label.SetActive(true);
+        }
+    }
 
 }
